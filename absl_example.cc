85d03d6737e69cc428fc59d6f283c356d568f577
Ryan Hitchman <rmmh@google.com>
2019-11-14 14:48:07 -0800

    Internal change
    PiperOrigin-RevId: 280518091

---
+++ absl_example.cc
@@ -0,0 +1,102 @@
+#include <pybind11/pybind11.h>
+
+#include "absl/strings/string_view.h"
+#include "absl/time/time.h"
+#include "absl/types/optional.h"
+#include "pybind11_abseil/absl_casters.h"
+
+namespace pybind11 {
+namespace test {
+
+absl::Duration MakeDuration(double secs) { return absl::Seconds(secs); }
+
+bool CheckDuration(const absl::Duration& duration, double secs) {
+  return duration == MakeDuration(secs);
+}
+
+absl::Time MakeTime(double secs) {
+  int64 int_secs = static_cast<int64>(secs);
+  int64 int_microsecs = static_cast<int64>((secs - int_secs) * 1e6);
+  return absl::FromUnixSeconds(int_secs) + absl::Microseconds(int_microsecs);
+}
+
+bool CheckDatetime(const absl::Time& datetime, double secs) {
+  return datetime == MakeTime(secs);
+}
+
+bool CheckSpan(absl::Span<const int32> span, const std::vector<int32>& values) {
+  if (span.size() != values.size()) return false;
+  for (int i = 0; i < span.size(); ++i) {
+    if (span[i] != values[i]) return false;
+  }
+  return true;
+}
+
+// Since a span does not own its elements, we must create a class to own them
+// and persist beyond the function that constructs the span for testing.
+class VectorContainer {
+ public:
+  absl::Span<const int32> MakeSpan(const std::vector<int32>& values) {
+    values_ = values;
+    return values_;
+  }
+
+ private:
+  std::vector<int32> values_;
+};
+
+bool CheckStringView(absl::string_view view, const string& values) {
+  return view == values;
+}
+
+// Since a string view does not own its data, we must create a class to own
+// them and persist beyond the function that constructs the span for testing.
+class StringContainer {
+ public:
+  absl::string_view MakeStringView(const string& values) {
+    values_ = values;
+    return values_;
+  }
+
+ private:
+  string values_;
+};
+
+bool CheckOptional(const absl::optional<int> optional, bool given, int value) {
+  if (!given && !optional.has_value()) return true;
+  if (given && optional.has_value() && optional.value() == value) return true;
+  return false;
+}
+
+absl::optional<int> MakeOptional() { return absl::nullopt; }
+absl::optional<int> MakeOptional(int value) { return value; }
+
+PYBIND11_MODULE(absl_example, m) {
+  // absl::Time/Duration bindings.
+  m.def("make_duration", &MakeDuration, arg("secs"));
+  m.def("check_duration", &CheckDuration, arg("duration"), arg("secs"));
+  m.def("make_datetime", &MakeTime, arg("secs"));
+  m.def("check_datetime", &CheckDatetime, arg("datetime"), arg("secs"));
+
+  // absl::Span bindings.
+  m.def("check_span", &CheckSpan, arg("span"), arg("values"));
+  class_<VectorContainer>(m, "VectorContainer")
+      .def(init())
+      .def("make_span", &VectorContainer::MakeSpan, arg("values"));
+
+  // absl::string_view bindings.
+  m.def("check_string_view", &CheckStringView, arg("view"), arg("values"));
+  class_<StringContainer>(m, "StringContainer")
+      .def(init())
+      .def("make_string_view", &StringContainer::MakeStringView, arg("values"));
+
+  // absl::optional bindings.
+  m.def("check_optional", &CheckOptional, arg("optional") = absl::nullopt,
+        arg("given") = false, arg("value") = 0);
+  m.def("make_optional", (absl::optional<int>(*)()) & MakeOptional);
+  m.def("make_optional", (absl::optional<int>(*)(int)) & MakeOptional,
+        arg("value"));
+}
+
+}  // namespace test
+}  // namespace pybind11
---
+++ absl_test.py
@@ -0,0 +1,103 @@
+"""Tests for absl pybind11 casters."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import datetime
+import numpy as np
+
+from google3.testing.pybase import googletest
+from google3.testing.pybase import parameterized
+from pybind11_abseil import absl_example
+
+
+class AbslTimeTest(googletest.TestCase):
+  SECONDS_IN_DAY = 24 * 60 * 60
+  POSITIVE_SECS = 3 * SECONDS_IN_DAY + 2.5
+  NEGATIVE_SECS = -3 * SECONDS_IN_DAY + 2.5
+  TEST_DATETIME = datetime.datetime(2000, 1, 2, 3, 4, 5, int(5e5))
+  TEST_DATE = datetime.date(2000, 1, 2)
+
+  def test_return_positive_duration(self):
+    duration = absl_example.make_duration(self.POSITIVE_SECS)
+    self.assertEqual(duration.days, 3)
+    self.assertEqual(duration.seconds, 2)
+    self.assertEqual(duration.microseconds, 5e5)
+
+  def test_return_negative_duration(self):
+    duration = absl_example.make_duration(self.NEGATIVE_SECS)
+    self.assertEqual(duration.days, -3)
+    self.assertEqual(duration.seconds, 2)
+    self.assertEqual(duration.microseconds, 5e5)
+
+  def test_pass_positive_duration(self):
+    duration = datetime.timedelta(seconds=self.POSITIVE_SECS)
+    self.assertTrue(absl_example.check_duration(duration, self.POSITIVE_SECS))
+
+  def test_pass_negative_duration(self):
+    duration = datetime.timedelta(seconds=self.NEGATIVE_SECS)
+    self.assertTrue(absl_example.check_duration(duration, self.NEGATIVE_SECS))
+
+  def test_return_datetime(self):
+    secs = self.TEST_DATETIME.timestamp()
+    self.assertEqual(self.TEST_DATETIME, absl_example.make_datetime(secs))
+
+  def test_pass_date(self):
+    secs = datetime.datetime(self.TEST_DATE.year, self.TEST_DATE.month,
+                             self.TEST_DATE.day).timestamp()
+    self.assertTrue(absl_example.check_datetime(self.TEST_DATE, secs))
+
+  def test_pass_datetime(self):
+    secs = self.TEST_DATETIME.timestamp()
+    self.assertTrue(absl_example.check_datetime(self.TEST_DATETIME, secs))
+
+
+class AbslSpanTest(parameterized.TestCase):
+
+  def test_return_span(self):
+    values = [1, 2, 3, 4]
+    container = absl_example.VectorContainer()
+    self.assertSequenceEqual(container.make_span(values), values)
+
+  @parameterized.named_parameters(('list', [2, 4, 6]), ('tuple', (1, 3, 5, 7)),
+                                  ('numpy', np.array([7, 8, 9])))
+  def test_pass_span_from(self, values):
+    # Pass values twice- one will be converted to a span, the other to a vector
+    # (which is known to work), and then they will be compared.
+    self.assertTrue(absl_example.check_span(values, values))
+
+
+class AbslStringViewTest(googletest.TestCase):
+  TEST_STRING = 'test string!'
+
+  def test_return_view(self):
+    container = absl_example.StringContainer()
+    self.assertSequenceEqual(
+        container.make_string_view(self.TEST_STRING), self.TEST_STRING)
+
+  def test_pass_string_view(self):
+    self.assertTrue(
+        absl_example.check_string_view(self.TEST_STRING, self.TEST_STRING))
+
+
+class AbslOptionalTest(googletest.TestCase):
+
+  def test_pass_default_nullopt(self):
+    self.assertTrue(absl_example.check_optional())
+
+  def test_pass_value(self):
+    self.assertTrue(absl_example.check_optional(5, True, 5))
+
+  def test_pass_none(self):
+    self.assertTrue(absl_example.check_optional(None, False))
+
+  def test_return_value(self):
+    self.assertEqual(absl_example.make_optional(5), 5)
+
+  def test_return_none(self):
+    self.assertIsNone(absl_example.make_optional())
+
+
+if __name__ == '__main__':
+  googletest.main()
