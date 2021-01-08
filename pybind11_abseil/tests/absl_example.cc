9b0531275ac8819b00b403549046918e8080947c
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-01-08 12:27:48 -0800

    Updating copyright notice dates.
    PiperOrigin-RevId: 350814845

--- pybind11_abseil/tests/absl_example.cc
+++ pybind11_abseil/tests/absl_example.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The Pybind Development Team. All rights reserved.
+// Copyright (c) 2019-2021 The Pybind Development Team. All rights reserved.
 //
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
