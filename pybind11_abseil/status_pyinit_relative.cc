b5a6cae5b2138c3c7f43de034b4f96fb0142281d
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-12-13 06:17:05 -0800

    Internal change
    PiperOrigin-RevId: 495002753

--- pybind11_abseil/status_pyinit_relative.cc
+++
@@ -1,8 +0,0 @@
-#include <Python.h>
-
-extern "C" PyObject*
-GooglePyInit_google3_third__party_pybind11__abseil_status();
-
-extern "C" PyObject* PyInit_status() {
-  return GooglePyInit_google3_third__party_pybind11__abseil_status();
-}
