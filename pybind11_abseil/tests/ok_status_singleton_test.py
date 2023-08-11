0af205efe662c2dae1a105e6f73dfb9c333771b7
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-08-11 13:27:19 -0700

    Improved handling of OSS vs Google-internal expected result.

--- pybind11_abseil/tests/ok_status_singleton_test.py
+++ pybind11_abseil/tests/ok_status_singleton_test.py
@@ -3,6 +3,8 @@
 # All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
+import sys
+
 from absl.testing import absltest
 
 from pybind11_abseil import ok_status_singleton
@@ -12,12 +14,11 @@ class OkStatusSingletonTest(absltest.TestCase):
 
   def test_singleton(self):
     cap = ok_status_singleton.OkStatusSingleton()
-    cap_repr = repr(cap)
-
-    # Modified to reflect the alternate behavior with CMake build
-    self.assertTrue(cap_repr.startswith(
-      '<capsule object "::absl::Status" at 0x') or cap_repr.startswith( 
-      '<pybind11_abseil.status.Status object at 0x'))
+    if 'pybind11_abseil.status' in sys.modules:  # OSS
+      expected_repr_cap = '<pybind11_abseil.status.Status object at 0x'
+    else:
+      expected_repr_cap = '<capsule object "::absl::Status" at 0x'
+    self.assertStartsWith(repr(cap), expected_repr_cap)
 
 
 if __name__ == '__main__':
