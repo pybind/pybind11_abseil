ea2282d11c0af3e5d36f4c9c00957ad4acd6d78c
pybind11_abseil authors <no-reply@google.com>
2024-02-22 21:36:14 -0800

    Add MODULE.bazel Test Python dependencies now use rules_python Update deps
    PiperOrigin-RevId: 609605937

--- scripts/build_and_run_tests_bazel.sh
+++ scripts/build_and_run_tests_bazel.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# The following scripts:
-# - updates the frozen dependencies
-# - builds and runs tests
+# The following script builds and runs tests
 
 set -e  # exit when any command fails
 set -x  # Prints all executed commands
@@ -21,12 +19,7 @@ fi
 
 echo "Building and testing in $PWD using 'python' (version $PYVERSION)."
 
-export PYTHON_BIN_PATH=`which python3`
-export PYTHON_LIB_PATH=`python3 -c "import sysconfig; print(sysconfig.get_path('include'))"`
-echo "Using PYTHON_BIN_PATH: $PYTHON_BIN_PATH"
-echo "Using PYTHON_LIB_PATH: $PYTHON_LIB_PATH"
-
-bazel clean --expunge
+bazel clean --expunge # Force a dep update
 
 BAZEL_CXXOPTS="-std=c++17" bazel test ... --test_output=errors "$@" --enable_bzlmod
 BAZEL_CXXOPTS="-std=c++20" bazel test ... --test_output=errors "$@" --enable_bzlmod
