b8ff43dbd70e96938cb8bc20000eff12f5fd5878
Corentin Le Molgat <corentinl@google.com>
2025-01-03 11:47:12 +0100

    scripts: add c++14 test
    note: abseil 20240722 and Protbuf v29.x should be the last version supporting c++14
    ref: https://github.com/google/oss-policies-info/commit/40084d5c4f2c1f79a1aaf1bcb6fc52c58465df89

--- scripts/build_and_run_tests_bazel.sh
+++ scripts/build_and_run_tests_bazel.sh
@@ -21,8 +21,10 @@ echo "Building and testing in $PWD using 'python' (version $PYVERSION)."
 
 bazel clean --expunge # Force a deep update
 
+bazel test --cxxopt=-std=c++14 ... --test_output=errors "$@" --enable_bzlmod
 bazel test --cxxopt=-std=c++17 ... --test_output=errors "$@" --enable_bzlmod
 bazel test --cxxopt=-std=c++20 ... --test_output=errors "$@" --enable_bzlmod
 
+bazel test --cxxopt=-std=c++14 ... --test_output=errors "$@" --noenable_bzlmod --enable_workspace
 bazel test --cxxopt=-std=c++17 ... --test_output=errors "$@" --noenable_bzlmod --enable_workspace
 bazel test --cxxopt=-std=c++20 ... --test_output=errors "$@" --noenable_bzlmod --enable_workspace
